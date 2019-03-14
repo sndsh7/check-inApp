a7a12695a65f606d581977cd54de5fe17f3395d9
Vishesh Pandey <developer.vishesh@gmail.com>
2019-03-14 21:09:07 +0530

    added my app instance

---
+++ app/src/main/java/co/in/socailbuzz/socialact/MyApplication.java
@@ -0,0 +1,42 @@
+package co.in.socailbuzz.socialact;
+
+import android.app.Application;
+
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class MyApplication extends Application {
+
+    private static MyApplication mInstance;
+    private static Retrofit retrofit;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        mInstance = this;
+        configureRetrofit();
+    }
+
+    public static MyApplication getInstance(){
+        return mInstance;
+    }
+
+    public static Retrofit getNetworkInstance(){
+        return retrofit;
+    }
+
+    private void configureRetrofit() {
+        HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
+        interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+        OkHttpClient.Builder builder = new OkHttpClient.Builder();
+        builder.addInterceptor(interceptor);
+
+        retrofit = new Retrofit.Builder()
+                .baseUrl(Api.ENDPOINT)
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(builder.build())
+                .build();
+    }
+}
