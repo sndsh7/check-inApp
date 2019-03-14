765d188ca8517e9436b8989af7ee4b3e4bb5820a
Vishesh Pandey <developer.vishesh@gmail.com>
2019-03-14 21:07:51 +0530

    added my app instance

--- app/src/main/java/co/in/socailbuzz/socialact/DataAdapter.java
+++ app/src/main/java/co/in/socailbuzz/socialact/DataAdapter.java
@@ -20,36 +20,22 @@ public class DataAdapter {
     }
 
     public static DataAdapter getInstance() {
-        if (mInstance == null) {
+        if (mInstance == null)
             mInstance = new DataAdapter();
-        }
-
         return mInstance;
     }
 
     public DataSource getDataSource() {
-        if (dataSource == null) {
+        if (dataSource == null)
             dataSource = new DataSource();
-        }
         return dataSource;
     }
 
     public class DataSource {
-        private Retrofit retrofit;
         private Api api;
 
         DataSource() {
-            HttpLoggingInterceptor interceptor=new HttpLoggingInterceptor();
-            interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
-            OkHttpClient.Builder builder=new OkHttpClient.Builder();
-            builder.addInterceptor(interceptor);
-
-            retrofit = new Retrofit.Builder()
-                    .baseUrl(Api.ENDPOINT)
-                    .addConverterFactory(GsonConverterFactory.create())
-                    .client(builder.build())
-                    .build();
-            api = retrofit.create(Api.class);
+            api = MyApplication.getNetworkInstance().create(Api.class);
         }
 
         public void getUsers(final UserCallback callback) {
@@ -60,15 +46,17 @@ public class DataAdapter {
                             if (response.isSuccessful())
                                 callback.onUsers(response.body().getData());
                             else
-                                callback.onFailed("Unable to get Users");
+                                callback.onFailed(MyApplication.getInstance().getString(R.string.fail_fetch_user));
                         }
 
                         @Override
                         public void onFailure(Call<Data> call, Throwable t) {
-                            callback.onFailed("Unable to get Users" +t.getMessage());
+                            callback.onFailed(MyApplication.getInstance().getString(R.string.fail_fetch_user) + t.getMessage());
                         }
                     });
         }
+
+
 
 
     }
